bf80ea2213a6b6514c9465475e0a4baa6f470170
Dev Nome <63138189+NumanKhan0103@users.noreply.github.com>
2024-08-26 11:21:25 +0500

    add home, about and user page

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,9 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../view/Home.vue';
 import About from '../view/About.vue';
+import Users from '../view/Users.vue';
 
-
-// routes
 const routes = [
     {
         path: '/',
@@ -15,15 +14,16 @@ const routes = [
         name: 'About',
         component: About
     },
-
+    {
+        path: '/user',
+        name: 'Users',
+        component: Users
+    },
 ]
 
 const router = createRouter({
-
     history: createWebHistory(),
     routes,
 });
 
-export default {
-
-}
+export default router;
