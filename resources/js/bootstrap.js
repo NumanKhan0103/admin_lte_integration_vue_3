f3e6695542e64053b7fe0488ffb9e6ccc19aee85
Dev Nome <63138189+NumanKhan0103@users.noreply.github.com>
2024-08-26 10:28:15 +0500

    import js and css file and link it

---
+++ resources/js/bootstrap.js
@@ -0,0 +1,8 @@
+import './assets/jquery.js';
+import './assets/bootstrap.js';
+import './assets/admin.js';
+
+import axios from 'axios';
+window.axios = axios;
+
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
